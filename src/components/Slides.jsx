f4fd48ecb4780645e3c53fa6819c24e77dd17962
Dattatray8 <dattatraymahindrakar@gmail.com>
2025-02-27 21:27:03 +0530

    update

--- src/components/Slides.jsx
+++ src/components/Slides.jsx
@@ -1,9 +1,23 @@
 import './Slides.css';
+import iphonead from '../images/iphonead.jpg';
+import laptopad from '../images/laptopad.webp';
+import headphonead from '../images/headphonead.webp';
+import { SlArrowLeft } from "react-icons/sl";
+import { SlArrowRight } from "react-icons/sl";
 
 function Slides(){
+    let images = [iphonead, laptopad, headphonead];
+    let index = 0;
+
+    function next() { index = (index + 1) % images.length; updateImage(); }
+    function prev() { index = (index - 1 + images.length) % images.length; updateImage(); }
+    function updateImage() { document.getElementById("slider").src = images[index]; }
+    setInterval(next, 5000);
     return (
         <div className="slide-container">
-
+            <img src={iphonead} alt="" id='slider'/>
+            <button id='btn-left' onClick={prev}><SlArrowLeft /></button>
+            <button id='btn-right' onClick={next}><SlArrowRight /></button>
         </div>
     );
 }
